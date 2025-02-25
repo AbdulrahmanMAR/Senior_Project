d6dc435e2ff30d773243d37422499856af97d6dc
AbdulrahmanMAR <yaya200666@gmail.com>
2025-02-26 01:24:07 +0300

    Deleted the keypoint extraction method. The way it was set up made so it doesn't really work with our translation program.

--- Hand_Tracking_Functions.py
+++ Hand_Tracking_Functions.py
@@ -20,35 +20,3 @@ def Landmark_Drawing(image, Landmarks_Results):
     if Landmarks_Results.multi_hand_landmarks is not None:
         for hand_landmarks in Landmarks_Results.multi_hand_landmarks:
             MP.solutions.drawing_utils.draw_landmarks(image, hand_landmarks, MP.solutions.hands.HAND_CONNECTIONS)
-
-#Fucntion to extract landmark keypoints as numpy array
-def Extract_Keypoints(Landmarks_Results):
-
-    #Initialize NP arrays
-    handl_Keypoints = NP.empty((0, 21), int)
-    handr_Keypoints = NP.empty((0, 21), int)
-    #Initialize hand label (left or right)
-    hand_label = str
-
-    #Find the hand label from the results to check which hand is detected
-    #This isn't useful when showing both hands as the app focuses on one but does detect both
-    #the main use of this will be to concatenate the keypoints correctly during dataset processing
-    if Landmarks_Results.multi_handedness is not None:
-        for hand in Landmarks_Results.multi_handedness:
-            hand_label = hand.classification[0].label
-
-    #Extracting keypoints from hands if present
-    if Landmarks_Results.multi_hand_landmarks is not None:
-        #Checks if the handlandmarks are for the left hand and extracts the keypoints
-        for hand_landmarks in Landmarks_Results.multi_hand_landmarks:
-            handl_Keypoints = NP.append(handl_Keypoints, NP.array([[LM.x, LM.y, LM.z] for LM in hand_landmarks.landmark]).flatten()
-                if hand_label == "Left" else NP.zeros(63))
-        # Checks if the handlandmarks are for the right hand and extracts the keypoints
-        for hand_landmarks in Landmarks_Results.multi_hand_landmarks:
-            handr_Keypoints = NP.append(handr_Keypoints, NP.array([[LM.x, LM.y, LM.z] for LM in hand_landmarks.landmark]).flatten()
-                if hand_label == "Right" else NP.zeros(63))
-
-    #Concatenate left and right hand keypoints
-    keypoints = NP.concatenate([handl_Keypoints, handr_Keypoints])
-
-    return keypoints
